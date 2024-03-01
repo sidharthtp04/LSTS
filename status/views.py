9f222e094b929132d31fc0f0ffe554bf2165927b
sidharthtp04 <sidharthtp2004@gmail.com>
2024-03-01 11:14:40 +0530

    table

--- status/views.py
+++ status/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # Perform authentication
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            # If authentication is successful, log the user in
+            login(request, user)
+            return redirect('home')  # Redirect to the home page or any other desired page
+        else:
+            # If authentication fails, display an error message
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    
+    return render(request, 'login.html')
+@csrf_exempt
 def home(request):
-    return HttpResponse('hello')
+    return render(request,'computer/base.html')
 
 def computer(request):
     cs={
@@ -52,5 +75,10 @@ def display(request):
         'details':computers.objects.all()
     }
     return render(request,'computer/display.html',details)
+
+
 def base(request):
     return render(request,'computer/base.html')
+
+
+
